05281561f3df27c67b632c69e9ef896dbc2a7826
Stephanie Briones <smbriones@gmail.com>
2015-09-20 12:20:04 -0500

    Adds Exercise 2.24: Odd or Even

---
+++ chapter-2/exercise-2.24.c
@@ -0,0 +1,22 @@
+// Exercises 2.21
+// (Odd or Even) Write a program that reads an integer and determines and prints
+// whether it is odd or even. [Hint: Use the remainder operater. Any even number
+// is a multiple of two. Any multiple of two leaves a remainder of zero when
+// divided by 2.]
+
+#include <stdio.h>
+
+int main() {
+  int number;
+
+  printf( "Enter an integer: \n" );
+  scanf( "%d", &number );
+
+  if ( number % 2 == 0 ) {
+    printf( "%d is an even number.\n", number );
+  } else {
+    printf( "%d is an odd number. \n", number );
+  }
+  
+  return 0;
+}
