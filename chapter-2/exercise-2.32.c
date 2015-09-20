800ea35feb43c307627ca5259c2074fa3fdea47a
Stephanie Briones <smbriones@gmail.com>
2015-09-20 16:27:34 -0500

    Adds BMI Calculator

---
+++ chapter-2/exercise-2.32.c
@@ -0,0 +1,29 @@
+// Exercises 2.26
+// Body Mass Index Calculator
+
+#include <stdio.h>
+
+int main() {
+  int weightInPounds, heightInInches, BMI, height, weight;
+
+  printf( "Enter your weight in pounds: \n" );
+  scanf( "%d", &weightInPounds );
+
+  printf( "Enter your height in inches: \n" );
+  scanf( "%d", &heightInInches );
+
+  weight = weightInPounds * 703;
+  height = heightInInches * heightInInches;
+
+  BMI = weight / height;
+
+  printf( "Your BMI is: %d\n", BMI );
+
+  printf( "BMI Values\n" );
+  printf( "Underweight:\tless than 18.5\n" );
+  printf( "Normal:\t\tbetween 18.5 and 24.9\n" );
+  printf( "Overweight:\tbetween 25 and 29.9\n" );
+  printf( "Obese:\t\t30 or greater\n" );
+
+  return 0;
+}
