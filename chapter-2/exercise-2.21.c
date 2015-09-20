74ad4b5979c227d5097faf01ebcf927972038bcf
Stephanie Briones <smbriones@gmail.com>
2015-09-20 11:19:12 -0500

    Adds Exercise 2.21: Shapes with Asterisks

---
+++ chapter-2/exercise-2.21.c
@@ -0,0 +1,15 @@
+// Exercises 2.21
+// (Shapes with Asterisks) Write a program that prints the following shapes
+// with asterisks.
+
+#include <stdio.h>
+
+int main() {
+
+  printf("*********\n*       *\n*       *\n*       *\n*       *\n*       *\n*       *\n*       *\n*********\n");
+  printf("   ***   \n *     *\n*       *\n*       *\n*       *\n*       *\n*       *\n *     *\n   ***   \n");
+  printf("  *  \n *** \n*****\n  *  \n  *  \n  *  \n  *  \n  *  \n  *  \n");
+  printf("    *    \n   * *   \n  *   *  \n *     * \n*       *\n *     * \n  *   *  \n   * *   \n    *    \n");
+
+  return 0;
+}
